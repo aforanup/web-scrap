d8e92ba0288d1e057b8b1cf1757c49a59da4101e
aforanup <aforanupn@gmail.com>
2022-07-31 18:39:18 +0545

    deployment branch to deploy on heroku

--- core/settings.py
+++ core/settings.py
@@ -90,12 +90,8 @@ CORS_ALLOW_HEADERS = ["*"]
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("POSTGRES_DB"),
-        "USER": config("POSTGRES_USER"),
-        "PASSWORD": config("POSTGRES_PASSWORD"),
-        "HOST": config("POSTGRES_HOST"),
-        "PORT": config("POSTGRES_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "blog",
     }
 }
 
